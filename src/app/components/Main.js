069dc2ce38aa3dd3fe0b791630ab2cbc1fce8904
samuel <samueltorochagas@gmail.com>
2024-07-17 11:00:30 -0400

    Implementando o componente ErroFetch - Requisito #5

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -2,19 +2,29 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Loading from "./Loading";
+import ErroFetch from "./ErroFecth";
 
 export default function Main() {
   const [listaFilmes, setFilmes] = useState([]);
+  const [erro, setErro] = useState(false)
 
   useEffect(() => {
     const getFilmes = async () => {
+     try{
       const response = await fetch("http://localhost:3000/api/");
       const data = await response.json();
       setFilmes(data);
-    };
-    getFilmes();
-  }, []);
+    }
+    catch{
+      setErro(true)
+    }
+  }
+    getFilmes()
+  }, [])
 
+  if(erro == true){
+    return <ErroFetch/>
+  }
 
   if (!listaFilmes.length) {
     return <Loading />;
