0c6bdeda75ca1bc4e886bd323eb90d8c3f122daf
samuel <samueltorochagas@gmail.com>
2024-07-18 10:12:36 -0400

    Implementei a Busca

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -5,10 +5,14 @@ import Loading from "./Loading";
 import ErroFetch from "./ErroFecth";
 import Link from "next/link";
 import styles from "../styles/main.module.css";
+import Image from "next/image";
+
 
 export default function Main() {
   const [listaFilmes, setFilmes] = useState([]);
-  const [erro, setErro] = useState(false)
+  const [erro, setErro] = useState(false);
+  const [listaToda, setListaToda] = useState([]);
+  const [textSearch, setTextSearch] = useState("");
 
   useEffect(() => {
     const getFilmes = async () => {
@@ -16,6 +20,7 @@ export default function Main() {
       const response = await fetch("http://localhost:3000/api/");
       const data = await response.json();
       setFilmes(data);
+      setListaToda(data);
     }
     catch{
       setErro(true)
@@ -24,16 +29,37 @@ export default function Main() {
     getFilmes()
   }, [])
 
+  const search = (text) => {
+    setTextSearch(text);
+    if (text === "") {
+      setFilmes(listaToda);
+      return;
+    }
+    const newList = listaToda.filter((filme) =>
+      filme.nome.toUpperCase().includes(text.toUpperCase().trim())
+    );
+    setFilmes(newList);
+  };
+
+
   if(erro == true){
     return <ErroFetch/>
   }
 
-  if (!listaFilmes.length) {
-    return <Loading />;
+  if (listaToda[0] == null) {
+    return  <Loading />
   }
-  
+
   return (
     <main className={styles.main}>
+       <div className={styles.searchcontainer}>
+          <input
+            type="text"
+            value={textSearch}
+            onChange={(event) => search(event.target.value)}
+            className={styles.searchinput}
+          />
+        </div>
       <div className={styles.grid}>
         {listaFilmes.map((filme) => (
           <div className={styles.card} key={filme.id}>
